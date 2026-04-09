f804c881db914ce4a1345dd7989e04aab662c44e
debian <contact@bciet.xyz>
2026-04-09 15:38:55 +0200

    last commit i swear

--- script_deploiement.sh
+++ script_deploiement.sh
@@ -1,39 +1,38 @@
 #!/bin/bash
 
-set -e
-
 apt update && apt upgrade -y
 apt install curl apache2 php php-mysql mariadb-server -y
 
 install_wordpress() {
-	cd /var/www/
-	echo "Install wordpress v$version"
-	wget https://wordpress.org/wordpress-"$version".tar.gz -o ./wordpress.tar.gz
-	tar xvf wordpress.tar.gz
-	cd wordpress
-	sed -i "s/username_here/$username"
-	sed -i "s/password_here/$password"
-	sed -i "s/database_name_here/$database"
-	a2ensite wordpress
-	systemctl restart apache2
+        cd /var/www/
+        echo "Install wordpress v$version"
+        wget https://wordpress.org/wordpress-"$version".tar.gz -O ./wordpress.tar.gz
+        tar xvf wordpress.tar.gz
+        cd wordpress
+        sed -i "s/username_here/$user/" wp-config-sample.php
+        sed -i "s/password_here/$password/" wp-config-sample.php
+        sed -i "s/database_name_here/$database/" wp-config-sample.php
+        cp wp-config-sample.php wp-config.php
+        #a2ensite wordpress
+        systemctl restart apache2
 }
 
 config_database() {
 
-	mysql -u root << EOL
-create database if not exists "$database";
-create user if not exists "$user"@localhost identified by "$password";
-grant all privileges on "$database".* to "$user"@localhost;
+        mysql -u root << "EOL"
+create database if not exists $database;
+create user if not exists $user@localhost identified by "$password";
+grant all privileges on $database.* to $user@localhost;
 flush privileges;
 EOL
 
 }
 
-while getopts "d:v:u:p" option; do
+while getopts "d:v:u:p:" option; do
   case $option in
     d) database=$OPTARG ;;      # set database name
-    v) version=$OPTARG ;;      	# set wordpress version
-    u) user=$OPTARG ;; 		# set user name
+    v) version=$OPTARG ;;       # set wordpress version
+    u) user=$OPTARG ;;          # set user name
     p) password=$OPTARG ;;      # set password
     *) echo
        echo "Invalid $OPTARG option"
@@ -43,3 +42,4 @@ done
 
 config_database
 install_wordpress
+
