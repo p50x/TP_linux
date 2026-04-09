252d69f4328769ea0d236e6f7cd51748deea7e35
root <contact@bciet.xyz>
2026-04-09 16:33:13 +0200

    New commit 3

--- script_deploiement.sh
+++ script_deploiement.sh
@@ -5,7 +5,7 @@ apt install curl apache2 php php-mysql mariadb-server -y
 
 conf_apache2() {
 	cd /etc/apache2/sites-available
-	cat << "EOF" > $site-wordpress.conf
+	cat << EOF > $site-wordpress.conf
 <VirtualHost *:80>
 	ServerName wordpress.$site.lan
 	DocumentRoot /var/www/$site/wordpress
@@ -24,7 +24,7 @@ install_wordpress() {
         cd wordpress
         sed -i "s/username_here/$user/" wp-config-sample.php
         sed -i "s/password_here/$password/" wp-config-sample.php
-        sed -i "s/database_name_here/$database/" wp-config-sample.php
+        sed -i "s/database_name_here/$site/" wp-config-sample.php
         cp wp-config-sample.php wp-config.php
 	systemctl restart apache2
 }
@@ -32,9 +32,9 @@ install_wordpress() {
 config_database() {
 
         mysql -u root << EOL
-create database if not exists $site-$database;
+create database if not exists $site;
 create user if not exists $user@localhost identified by "$password";
-grant all privileges on $site-$database.* to $user@localhost;
+grant all privileges on $site.* to $user@localhost;
 flush privileges;
 EOL
 
@@ -42,7 +42,6 @@ EOL
 
 while getopts "d:v:u:p:s:" option; do
   case $option in
-    d) database=$OPTARG ;;      # set database name
     v) version=$OPTARG ;;       # set wordpress version
     u) user=$OPTARG ;;          # set user name
     p) password=$OPTARG ;;      # set password
